f9383a8c3127ce47b2d04f3c56f20faa618632ac
hatim997 <laravel.supersoft@gmail.com>
2025-05-11 18:52:39 +0500

    small changes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="{{ route('dashboard') }}" class="app-brand-link">
+        <a href="{{ route('frontend.dashboard') }}" class="app-brand-link">
             <span class="app-brand-logo demo" style="width: 60px !important;">
                 <img style="width: 60px !important;" src="{{ asset(\App\Helpers\Helper::getLogoDark()) }}" alt="{{env('APP_NAME')}}">
             </span>
